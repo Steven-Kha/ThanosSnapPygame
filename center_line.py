f2a2dc6b26a03f5cf41ed6d4473a8ec049521868
Steven-Kha <stevenvkha@gmail.com>
2019-04-03 10:49:15 -0700

    removed comments

--- center_line.py
+++ center_line.py
@@ -19,46 +19,8 @@ class Center_Line(Sprite):
 
         self.color = ai_settings.center_line_color
 
-        #self.height = float(ai_settings.left_paddle_height)
-
-        #left_paddle starts at center left of screen
         self.rect.centery = self.screen_rect.centery
         self.rect.centerx = self.screen_rect.centerx
-        #print("Left Paddle position: " + str(self.rect))
-
-        # # Store a decimal value for the ship's center.
-        # self.center = float(self.rect.centery)
-        #
-        # # Movement flag for continuous movement
-        # self.moving_up = False
-        # self.moving_down = False
-
-    # def update(self, ai_settings):
-    #     """Update the ship's position based on the movement flag."""
-    #     # Update the ship's center value, not the rect.
-    #
-    #     #stop paddle from going off the top edge
-    #     # Y axis gets smaller when it goes up!
-    #     if self.moving_up and self.rect.top > 0:
-    #         self.center -= self.ai_settings.left_paddle_speed_factor
-    #         # print("Left Paddle position: " + str(self.rect))
-    #         # print("self.rect.top: " + str(self.rect.top) + " > "
-    #         #     "self.screen_rect.top: " + str(self.screen_rect.top))
-    #
-    #     # stop paddle from going off the bottom edge y axis gettings
-    #     # bigger when it moves down! When it reaches the end it is 800
-    #     if self.moving_down and self.rect.bottom < 800:
-    #         self.center += self.ai_settings.left_paddle_speed_factor
-    #
-    #         # print("self.rect.bottom: " + str(self.rect.bottom) + "< 800")
-    #         # print("Left Paddle position: " + str(self.rect))
-    #     # Update rect object from self.center.
-    #
-    #     self.rect.centery = self.center
-
-    # def center_left_paddle(self):
-    #     """Center the ship on the screen."""
-    #     self.center = self.screen_rect.midleft
 
 
     def draw_center_line(self):
