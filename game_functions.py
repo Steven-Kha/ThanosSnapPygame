b49870a0f85ad4c5b6c4b3b7e104d13d00e6ee3f
Steven-Kha <stevenvkha@gmail.com>
2019-04-03 10:30:21 -0700

    first commit

---
+++ game_functions.py
@@ -0,0 +1,16 @@
+import sys
+import pygame
+
+
+def check_events(ai_settings, screen):
+    """Respond to keypresses and mouse events."""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen):
+    # Redraw the screen during each pass through the loop.
+    screen.fill(ai_settings.bg_color)
+
+    pygame.display.flip()
+
