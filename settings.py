9c8e639aecc4f12e2ca95fc65d15d35fc1a88a3e
Steven-Kha <stevenvkha@gmail.com>
2019-04-04 04:51:11 -0700

    good projects to relearn pygame

--- settings.py
+++ settings.py
@@ -5,9 +5,11 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
+        #thanos purple lol
         self.bg_color = (167, 136, 168)
 
         self.center_line_width = 6
         self.center_line_height = self.screen_height
+        #more thanos purple lol
         self.center_line_color = (111, 60, 137)
 
